facf38a7c70089879a3808f50437c89e06331fbc
anjali.agarwal <anjali.aggarwal@gojek.com>
2023-05-26 11:54:20 +0530

    fix: search and group asset test case fix

--- internal/store/elasticsearch/discovery_search_repository.go
+++ internal/store/elasticsearch/discovery_search_repository.go
@@ -155,27 +155,28 @@ func (repo *DiscoveryRepository) buildSuggestQuery(cfg asset.SearchConfig) (io.R
 func (repo *DiscoveryRepository) buildTextQuery(q *elastic.BoolQuery, cfg asset.SearchConfig) {
 	boostedFields := []string{"urn^10", "name^5"}
 
+	q.Should(
+		elastic.NewMultiMatchQuery(cfg.Text, boostedFields...),
+		elastic.NewMatchPhraseQuery("message", cfg.Text),
+	)
+
 	if cfg.Flags.DisableFuzzy {
 		q.Should(
-			elastic.NewMultiMatchQuery(cfg.Text, boostedFields...),
 			elastic.NewMultiMatchQuery(cfg.Text),
 			elastic.NewMultiMatchQuery(cfg.Text, boostedFields...).
 				Operator("and"),
-			elastic.NewMatchPhraseQuery("message", cfg.Text),
-			elastic.NewMultiMatchQuery(cfg.Text),
 		)
-		return
+	} else {
+		q.Should(
+			elastic.NewMultiMatchQuery(cfg.Text, boostedFields...).
+				Fuzziness("AUTO"),
+			elastic.NewMultiMatchQuery(cfg.Text).
+				Fuzziness("AUTO"),
+			elastic.NewMultiMatchQuery(cfg.Text, boostedFields...).
+				Operator("and").
+				Fuzziness("AUTO"),
+		)
 	}
-
-	q.Should(
-		elastic.NewMultiMatchQuery(cfg.Text, boostedFields...),
-		elastic.NewMultiMatchQuery(cfg.Text, boostedFields...).
-			Fuzziness("AUTO"),
-		elastic.NewMultiMatchQuery(cfg.Text).
-			Fuzziness("AUTO"),
-		elastic.NewMultiMatchQuery(cfg.Text, boostedFields...).
-			Operator("and"),
-	)
 }
 
 func (repo *DiscoveryRepository) buildMustMatchQueries(q *elastic.BoolQuery, cfg asset.SearchConfig) {
