413f0c5ff60ee604eacc9fe08067d9cbaead8bf0
Muhammad Luthfi Fahlevi <muhammad.fahlevi@gojek.com>
2025-07-14 14:24:22 +0700

    feat: add config in yaml example

--- internal/cleanup/config.go
+++ internal/cleanup/config.go
@@ -4,6 +4,6 @@ import "time"
 
 type Config struct {
 	DryRun     bool          `mapstructure:"dry_run" default:"true"`
-	ExpiryTime time.Duration `mapstructure:"expiry_time" default:"720h"`
-	Services   string        `mapstructure:"services"` // list of services separated by comma, "all" means all service
+	ExpiryTime time.Duration `mapstructure:"expiry_time" default:"720h0m0s"` // 30 days
+	Services   string        `mapstructure:"services"`                       // list of services separated by comma, "all" means all service
 }
