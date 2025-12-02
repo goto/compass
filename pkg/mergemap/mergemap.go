b23b8d0f9992792cabec1ccd91bd361fc43ab3fd
Lifosmin Simon <lifosmin.simon@gojek.com>
2025-12-02 21:30:39 +0700

    fix lint

--- pkg/mergemap/mergemap.go
+++ pkg/mergemap/mergemap.go
@@ -4,7 +4,7 @@ import (
 	"reflect"
 )
 
-var MaxDepth = 32
+const MaxDepth = 32
 
 // Merge recursively merges the src and dst maps. Key conflicts are resolved by
 // preferring src, or recursively descending, if both src and dst are maps.
