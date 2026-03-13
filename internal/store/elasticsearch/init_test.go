da17717404be9e9dddf836bb7d99b216566483c7
Frederick Wijayadi Susilo <frederick.wijayadisusilo@gmail.com>
2026-03-13 11:24:13 +0700

    chore: fix es timeout

--- internal/store/elasticsearch/init_test.go
+++ internal/store/elasticsearch/init_test.go
@@ -1,6 +1,10 @@
 package elasticsearch_test
 
 import (
+	"fmt"
+	"os"
+	"testing"
+
 	"github.com/goto/compass/internal/store/elasticsearch/testutil"
 )
 
@@ -9,17 +13,17 @@ var (
 	esTestServer  *testutil.ElasticsearchTestServer
 )
 
-// func TestMain(m *testing.M) {
-//	// TODO(Aman): this block makes it impossible to skip starting
-//	// an elasticsearch server. That means you can't run unit tests
-//	// standalone :/
-//	esTestServer = testutil.NewElasticsearchTestServer()
-//
-//	exitCode := m.Run()
-//
-//	if err := esTestServer.Close(); err != nil {
-//		fmt.Println("Error closing elasticsearch test server:", err)
-//		return
-//	}
-//	os.Exit(exitCode)
-//}
+func TestMain(m *testing.M) {
+	// TODO(Aman): this block makes it impossible to skip starting
+	// an elasticsearch server. That means you can't run unit tests
+	// standalone :/
+	esTestServer = testutil.NewElasticsearchTestServer()
+
+	exitCode := m.Run()
+
+	if err := esTestServer.Close(); err != nil {
+		fmt.Println("Error closing elasticsearch test server:", err)
+		return
+	}
+	os.Exit(exitCode)
+}
