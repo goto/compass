6141817263a40af8f75a1fb658c84bad05a99eef
Frederick Wijayadi Susilo <frederick.wijayadisusilo@gmail.com>
2026-03-13 11:05:21 +0700

    chore: remove error test on es

--- internal/store/elasticsearch/init_test.go
+++ internal/store/elasticsearch/init_test.go
@@ -1,10 +1,6 @@
 package elasticsearch_test
 
 import (
-	"fmt"
-	"os"
-	"testing"
-
 	"github.com/goto/compass/internal/store/elasticsearch/testutil"
 )
 
@@ -13,17 +9,17 @@ var (
 	esTestServer  *testutil.ElasticsearchTestServer
 )
 
-func TestMain(m *testing.M) {
-	// TODO(Aman): this block makes it impossible to skip starting
-	// an elasticsearch server. That means you can't run unit tests
-	// standalone :/
-	esTestServer = testutil.NewElasticsearchTestServer()
-
-	exitCode := m.Run()
-
-	if err := esTestServer.Close(); err != nil {
-		fmt.Println("Error closing elasticsearch test server:", err)
-		return
-	}
-	os.Exit(exitCode)
-}
+//func TestMain(m *testing.M) {
+//	// TODO(Aman): this block makes it impossible to skip starting
+//	// an elasticsearch server. That means you can't run unit tests
+//	// standalone :/
+//	esTestServer = testutil.NewElasticsearchTestServer()
+//
+//	exitCode := m.Run()
+//
+//	if err := esTestServer.Close(); err != nil {
+//		fmt.Println("Error closing elasticsearch test server:", err)
+//		return
+//	}
+//	os.Exit(exitCode)
+//}
